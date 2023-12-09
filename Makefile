e669034f5474b1243b051c556becd754467fc790
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-12-09 20:46:27 +0800

    build: add gitignore, Makefile, and cmd files
    - Added `.gitignore` file with new entries for `coverage.txt`, `dist`, `**/.DS_Store`, `bin`, and `release`
    - Added `Makefile` with build, install, test, and build commands for different platforms
    - Added `cmd/bitbucketServer2Gitea/main.go` file
    - Added `cmd/cmd.go` file
    - Added `cmd/version.go` file
    - Added `go.mod` file with required dependencies
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ Makefile
@@ -0,0 +1,41 @@
+GO ?= go
+EXECUTABLE := bitbucketServer2Gitea
+GOFILES := $(shell find . -type f -name "*.go")
+TAGS ?=
+LDFLAGS ?= -X 'github.com/appleboy/BitbucketServer2Gitea/cmd.Version=$(VERSION)' -X 'github.com/appleboy/BitbucketServer2Gitea/cmd.Commit=$(COMMIT)'
+
+ifneq ($(shell uname), Darwin)
+	EXTLDFLAGS = -extldflags "-static" $(null)
+else
+	EXTLDFLAGS =
+endif
+
+ifneq ($(DRONE_TAG),)
+	VERSION ?= $(DRONE_TAG)
+else
+	VERSION ?= $(shell git describe --tags --always || git rev-parse --short HEAD)
+endif
+COMMIT ?= $(shell git rev-parse --short HEAD)
+
+build: $(EXECUTABLE)
+
+$(EXECUTABLE): $(GOFILES)
+	$(GO) build -v -tags '$(TAGS)' -ldflags '$(EXTLDFLAGS)-s -w $(LDFLAGS)' -o bin/$@ ./cmd/$(EXECUTABLE)
+
+install: $(GOFILES)
+	$(GO) install -v -tags '$(TAGS)' -ldflags '$(EXTLDFLAGS)-s -w $(LDFLAGS)'
+
+test:
+	@$(GO) test -v -cover -coverprofile coverage.txt ./... && echo "\n==>\033[32m Ok\033[m\n" || exit 1
+
+build_linux_amd64:
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 $(GO) build -a -tags '$(TAGS)' -ldflags '$(EXTLDFLAGS)-s -w $(LDFLAGS)' -o release/linux/amd64/$(EXECUTABLE) ./cmd/$(EXECUTABLE)
+
+build_linux_arm64:
+	CGO_ENABLED=0 GOOS=linux GOARCH=arm64 $(GO) build -a -tags '$(TAGS)' -ldflags '$(EXTLDFLAGS)-s -w $(LDFLAGS)' -o release/linux/arm64/$(EXECUTABLE) ./cmd/$(EXECUTABLE)
+
+build_mac_intel:
+	CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 $(GO) build -a -tags '$(TAGS)' -ldflags '$(EXTLDFLAGS)-s -w $(LDFLAGS)' -o release/mac/intel/$(EXECUTABLE) ./cmd/$(EXECUTABLE)
+
+build_windows_64:
+	CGO_ENABLED=0 GOOS=windows GOARCH=amd64 $(GO) build -a -tags '$(TAGS)' -ldflags '$(EXTLDFLAGS)-s -w $(LDFLAGS)' -o release/windows/intel/$(EXECUTABLE).exe ./cmd/$(EXECUTABLE)
---
+++ cmd/cmd.go
@@ -0,0 +1,89 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/appleboy/com/file"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var rootCmd = &cobra.Command{
+	Short:        "A command line tool build with Golang to migrate a Bitbucket to Gitea.",
+	SilenceUsage: true,
+	Args:         cobra.MaximumNArgs(1),
+}
+
+// Used for flags.
+var (
+	cfgFile  string
+	replacer = strings.NewReplacer("-", "_", ".", "_")
+)
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/codegpt/.codegpt.yaml)")
+	rootCmd.AddCommand(versionCmd)
+
+	// hide completion command
+	rootCmd.CompletionOptions.HiddenDefaultCmd = true
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+		if !file.IsFile(cfgFile) {
+			// Config file not found; ignore error if desired
+			_, err := os.Create(cfgFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".cobra" (without extension).
+		configFolder := path.Join(home, ".config", "bitbucketServer2Gitea")
+		viper.AddConfigPath(configFolder)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".codegpt")
+		cfgFile = path.Join(configFolder, ".codegpt.yaml")
+
+		if !file.IsDir(configFolder) {
+			if err := os.MkdirAll(configFolder, os.ModePerm); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(replacer)
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			_, err := os.Create(cfgFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			// Config file was found but another error was produced
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+func Execute(ctx context.Context) {
+	if _, err := rootCmd.ExecuteContextC(ctx); err != nil {
+		os.Exit(1)
+	}
+}
