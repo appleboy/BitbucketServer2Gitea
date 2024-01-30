690632fce8692e0160211960c5d3aad9d03d26db
amix307 <amix307@mail.ru>
2024-01-30 17:05:42 +0300

    Update mirgation.go

--- migration/mirgation.go
+++ migration/mirgation.go
@@ -60,7 +60,6 @@ type CreateNewOrgOption struct {
 	Name        string
 	Description string
 	Public      bool
-	Permission  map[string][]string
 }
 
 // CreateNewOrg create new organization
@@ -74,20 +73,6 @@ func (m *migration) CreateNewOrg(opts CreateNewOrgOption) error {
 	if err != nil {
 		return err
 	}
-
-	m.Logger.Info("start migrate organization permission", "name", opts.Name)
-	for permission, users := range opts.Permission {
-		team, err := m.Gitea.CreateOrGetTeam(opts.Name, permission)
-		if err != nil {
-			return err
-		}
-		for _, user := range users {
-			err := m.Gitea.AddTeamMember(team.ID, user)
-			if err != nil {
-				return err
-			}
-		}
-	}
 	return nil
 }
 
@@ -98,7 +83,6 @@ type MigrateNewRepoOption struct {
 	CloneAddr   string
 	Description string
 	Private     bool
-	Permission  map[string][]string
 }
 
 // MigrateNewRepo migrate repository
@@ -119,19 +103,6 @@ func (m *migration) MigrateNewRepo(opts MigrateNewRepoOption) error {
 	if err != nil {
 		return err
 	}
-
-	m.Logger.Info("start migrate repo permission",
-		"owner", opts.Owner,
-		"name", opts.Name,
-	)
-	for permission, users := range opts.Permission {
-		for _, user := range users {
-			_, err := m.Gitea.AddCollaborator(opts.Owner, opts.Name, user, permission)
-			if err != nil {
-				return err
-			}
-		}
-	}
 	return nil
 }
 
@@ -148,77 +119,14 @@ func (m *migration) GetProjectData(projectKey string) (*ProjectResponse, error) 
 		return nil, err
 	}
 
-	permission := make(map[string][]string)
-	// check project user permission
-	users, err := m.Bitbucket.GetUsersPermissionFromProject(projectKey)
-	if err != nil {
-		return nil, err
-	}
-	for _, user := range users {
-		m.Logger.Debug("project permission",
-			"display", user.User.DisplayName,
-			"account", user.User.Name,
-			"permission", user.Permission,
-		)
-		_, err := m.Gitea.GreateOrGetUser(CreateUserOption{
-			SourceID:  m.Gitea.sourceID,
-			LoginName: strings.ToLower(user.User.Name),
-			Username:  user.User.Name,
-			FullName:  user.User.DisplayName,
-			Email:     user.User.EmailAddress,
-		})
-		if err != nil {
-			return nil, err
-		}
-		permission[user.Permission] = append(permission[user.Permission], strings.ToLower(user.User.Name))
-	}
-
-	// check project group permission
-	groups, err := m.Bitbucket.GetGroupsPermissionFromProject(projectKey)
-	if err != nil {
-		return nil, err
-	}
-	for _, group := range groups {
-		m.Logger.Debug("group permission for project",
-			"name", group.Group.Name,
-			"permission", group.Permission,
-		)
-
-		users, err := m.Bitbucket.GetUsersFromGroup(group.Group.Name)
-		if err != nil {
-			return nil, err
-		}
-		for _, user := range users {
-			m.Logger.Debug("user permission in group",
-				"display", user.DisplayName,
-				"account", user.Name,
-				"permission", group.Permission,
-				"group", group.Group.Name,
-			)
-			_, err := m.Gitea.GreateOrGetUser(CreateUserOption{
-				SourceID:  m.Gitea.sourceID,
-				LoginName: strings.ToLower(user.Name),
-				Username:  user.Name,
-				FullName:  user.DisplayName,
-				Email:     user.EmailAddress,
-			})
-			if err != nil {
-				return nil, err
-			}
-			permission[group.Permission] = append(permission[group.Permission], strings.ToLower(user.Name))
-		}
-	}
-
 	return &ProjectResponse{
 		Project:    org,
-		Permission: permission,
 	}, nil
 }
 
 // RepositoryResponse repository response
 type RepositoryResponse struct {
 	Repository bitbucketv1.Repository
-	Permission map[string][]string
 }
 
 // GetRepositoryData get repository data
@@ -228,70 +136,7 @@ func (m *migration) GetRepositoryData(projectKey, repoSlug string) (*RepositoryR
 		return nil, err
 	}
 
-	// check project group permission
-	groups, err := m.Bitbucket.GetGroupsPermissionFromRepo(projectKey, repoSlug)
-	if err != nil {
-		return nil, err
-	}
-
-	permission := make(map[string][]string)
-	for _, group := range groups {
-		m.Logger.Debug("group permission for repo",
-			"name", group.Group.Name,
-			"permission", group.Permission,
-		)
-
-		users, err := m.Bitbucket.GetUsersFromGroup(group.Group.Name)
-		if err != nil {
-			return nil, err
-		}
-		for _, user := range users {
-			m.Logger.Debug("user permission in repo",
-				"display", user.DisplayName,
-				"account", user.Name,
-				"permission", group.Permission,
-				"group", group.Group.Name,
-			)
-			_, err := m.Gitea.GreateOrGetUser(CreateUserOption{
-				SourceID:  m.Gitea.sourceID,
-				LoginName: strings.ToLower(user.Name),
-				Username:  user.Name,
-				FullName:  user.DisplayName,
-				Email:     user.EmailAddress,
-			})
-			if err != nil {
-				return nil, err
-			}
-			permission[group.Permission] = append(permission[group.Permission], strings.ToLower(user.Name))
-		}
-	}
-
-	// check repo user permission
-	users, err := m.Bitbucket.GetUsersPermissionFromRepo(projectKey, repoSlug)
-	if err != nil {
-		return nil, err
-	}
-	for _, user := range users {
-		m.Logger.Debug("repo permission",
-			"display", user.User.DisplayName,
-			"account", user.User.Name,
-			"permission", user.Permission,
-		)
-		_, err := m.Gitea.GreateOrGetUser(CreateUserOption{
-			SourceID:  m.Gitea.sourceID,
-			LoginName: strings.ToLower(user.User.Name),
-			Username:  user.User.Name,
-			FullName:  user.User.DisplayName,
-			Email:     user.User.EmailAddress,
-		})
-		if err != nil {
-			return nil, err
-		}
-		permission[user.Permission] = append(permission[user.Permission], strings.ToLower(user.User.Name))
-	}
-
 	return &RepositoryResponse{
 		Repository: repo,
-		Permission: permission,
 	}, nil
 }
