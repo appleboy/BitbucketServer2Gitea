f650f851a9bf14947f4868039b1ac43844066b60
appleboy <appleboy.tw@gmail.com>
2025-04-20 09:56:20 +0800

    refactor: clean up unused SCM integrations and related constants
    - Remove unused import of the Gitea SDK package
    - Delete Bitbucket permissions constants
    - Remove the DefaultUnits variable and its associated Gitea repo unit types
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- migration/constants.go
+++ migration/constants.go
@@ -1,7 +1,5 @@
 package migration
 
-import gsdk "code.gitea.io/sdk/gitea"
-
 const (
 	// Gitea permissions
 	GiteaRepoAdmin    = "admin"
@@ -11,26 +9,4 @@ const (
 	GiteaProjectWrite = "write"
 	GiteaProjectRead  = "read"
 	GiteaRepoCreate   = "create"
-
-	// Bitbucket permissions
-	BitbucketProjectAdmin = "PROJECT_ADMIN"
-	BitbucketProjectWrite = "PROJECT_WRITE"
-	BitbucketProjectRead  = "PROJECT_READ"
-	BitbucketRepoAdmin    = "REPO_ADMIN"
-	BitbucketRepoWrite    = "REPO_WRITE"
-	BitbucketRepoRead     = "REPO_READ"
-	BitbucketRepoCreate   = "REPO_CREATE"
 )
-
-var DefaultUnits = []gsdk.RepoUnitType{
-	gsdk.RepoUnitCode,
-	gsdk.RepoUnitIssues,
-	gsdk.RepoUnitExtIssues,
-	gsdk.RepoUnitExtWiki,
-	gsdk.RepoUnitPackages,
-	gsdk.RepoUnitProjects,
-	gsdk.RepoUnitPulls,
-	gsdk.RepoUnitReleases,
-	gsdk.RepoUnitWiki,
-	gsdk.RepoUnitActions,
-}
